22db5a0b8812f23a1c36caf6ca96e18fc0773f1b
namji <skawlgns07@naver.com>
2024-02-20 16:36:16 +0900

    feat: 폴더에 담겨져 있는 상품 조회하는 기능 구현
    폴더에 담겨있는 관심상품 조회하는 기능 추가 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -9,6 +9,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
     Page<Product> findAllByUser(User user, Pageable pageable);
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    /*
+    findAllByUserAndProductFolderList_FolderId 쿼리문으로 작성하면
+
+    select
+        p.id,
+        p.title,
+        pf.product_id,
+        pf.folder_id
+    from
+        product p left join product_folder pf
+    on
+        p.id = pf.product_id
+    where
+        p.user_id = 1
+    and
+        pf.folder_id = 3
+    order by
+        p.id
+    limit 0, 10;
+    이런식으로 작성
+
+     */
 }
