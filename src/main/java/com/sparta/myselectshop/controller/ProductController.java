c9368c125962731797a7443d67b9a766d35a80c9
namji <skawlgns07@naver.com>
2024-02-20 16:10:09 +0900

    feat:관심상품 폴더별로 저장 기능 구현
    폴더를 생성하는 기능 구현 후 상품을 폴더를 선택하여 그 폴더에 저장하여 관리할 수 있게 구현했습니다.

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -47,4 +47,14 @@ public class ProductController {
                 userDetails.getUser(),
                 page-1, size, sortBy, isAsc);
     }
+
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
 }
